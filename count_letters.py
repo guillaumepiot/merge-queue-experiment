8e41941ee968e73094845ed1cc32b1c1a82336ee
Guillaume Piot <guillaume@boughtbymany.com>
2022-02-22 12:49:05 +0000

    Make count_letters_fn

--- count_letters.py
+++ count_letters.py
@@ -1,22 +1,22 @@
 import sys
 import string
 
-def count_letters(input):
+def count_letters_fn(input):
     letters = [char for char in input if char in string.ascii_letters]
     count = len(letters)
 
     if count == 0:
-        print(f"Empty string!")
+        return f"Empty string!"
     elif count == 1:
-        print(f"There is {count} letter in this string.")
+        return f"There is {count} letter in this string."
     else:
-        print(f"There are {count} letters in this string.")
+        return f"There are {count} letters in this string."
 
 if __name__ == "__main__":
-
+    
     try:
         input = sys.argv[1]
-        count_letters(input)
+        print(count_letters_fn(input))
     except IndexError:
         print("No string submitted.")
 
